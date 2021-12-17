ad163d1bee9ed0bc35c5fe2c2a47b4bb6089fab0
TeckVo <94139588+TeckVo@users.noreply.github.com>
2021-12-16 20:17:50 -0500

    Update untitled4.py

--- untitled4.py
+++ untitled4.py
@@ -12,75 +12,13 @@ import altair as alt
 import streamlit as st
 import matplotlib.pyplot as plt
 import csv
-uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-     bytes_data = uploaded_file.read()
-     st.write("filename:", uploaded_file.name)
-     st.write(bytes_data)
-     
-def load_data(nrows):
-     data = pd.read_csv('C:/Users/Cu Chi/Dropbox/My PC (CuChi)/Desktop/Web-based GUI/Data_set/Discharging ESS.csv', nrows=nrows)
-     return data
-weekly_data = load_data(96)
-df_1 = pd.DataFrame(weekly_data[:96],columns = ['ESS1','ESS2'],
-                    index=pd.RangeIndex(100, name='x'))    
-df_1 = df_1.reset_index().melt('x', var_name='ESS', value_name='y')
-line_chart = alt.Chart(df_1).mark_line().encode(
-    alt.X('x', title='Time slot [min]'),
-    alt.Y('y', title='Discharging power [MW]'),
-    color='ESS:N').properties(title='ESS scheduling')
 
-st.altair_chart(line_chart)
-
-
-
-#def base_load(nrows):
-    #data = pd.read_csv('C:/Users/Cu Chi/Dropbox/My PC (CuChi)/Desktop/Web-based GUI/Data_set/Base load.csv', nrows=nrows)
-    #return data
-#load_demand = base_load (366)       
-df = pd.DataFrame(weekly_data[:365],columns = ['Time [day]','Base_load [p.u]'])
-c = alt.Chart(df).mark_line().encode(
-     x='Time [day]', y='Base_load [p.u]', tooltip=['Time [day]', 'Base_load [p.u]'])
-st.altair_chart(c, use_container_width=True)
-
-
-
-def load_data(nrows):
-     data = pd.read_csv('C:/Users/Cu Chi/Dropbox/My PC (CuChi)/Desktop/Web-based GUI/Data_set/Base load.csv', nrows=nrows)
-     return data
-weekly_data = load_data(365)
-exampledata = np.array(weekly_data[1:],dtype=np.float64)               
-xdata=exampledata[:,0]
-ydata=exampledata[:,1]
-fig, ax = plt.subplots()
-plt.plot(xdata,ydata)
-ax.set_xlabel("Time [day]")
-ax.set_ylabel("Base load [p.u]")            
-st.pyplot(fig)           
-
-import matplotlib.pyplot as plt
-import numpy as np
 
 arr = np.random.normal(1, 1, size=100)
 fig, ax = plt.subplots()
 ax.hist(arr, bins=20)
 
 st.pyplot(fig)
-
-
-
-
-
-#with open ('C:/Users/Cu Chi/Dropbox/My PC (CuChi)/Desktop/Web-based GUI/Data_set/Base load.csv','r') as i:
-    #rawdata = list(csv.reader(i,delimiter=","))
-#exampledata=np.array(rawdata[1:],dtype=np.float64)  
-#xdata=exampledata[:,0]
-#ydata=exampledata[:,1]
-#fig, ax = plt.subplots()
-#plt.plot(xdata,ydata)
-#ax.set_xlabel("Time [day]")
-#ax.set_ylabel("Base load [p.u]")            
-#st.pyplot(fig)           
 
             
 
